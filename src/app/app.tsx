a9202ff72e44cfe9eb53f967566e249478f775ef
jaxchow <jaxchow@gmail.com>
2021-11-28 16:45:37 +0800

    chore: initial project

---
+++ src/app/app.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [conn,setConn] = useState([])
+    useEffect(function(){
+      //@ts-ignore
+        window.dbsdk.initSqlite().then(function(result){
+            console.log("result",result)
+            //@ts-ignore
+            window.dbsdk.addConnection(["mysql","127.0.0.1","root","root"]).then(function(result){
+                console.log(result)
+                setConn(result)
+            }).catch(function(err:Error){
+                console.log("err:",err)
+            })
+        })  
+    },[])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dbsdk/sqlite3.ts
@@ -0,0 +1,44 @@
+import { Connection, ResultSet } from "any-db"
+import { getConnection } from "."
+
+const sqlite = require('any-db-sqlite3')
+
+let sqlite3conn: Connection = null;
+
+function createConnection(): Promise<any> {
+  return getConnection('sqlite3://:memory')
+}
+
+export function initSqlite3() {
+  return new Promise(function (resolve, reject) {
+    createConnection().then(function (conn: Connection) {
+      sqlite3conn = conn
+      const createTableStruct = `
+        CREATE TABLE IF NOT EXISTS connection(
+        id int,
+        name varchar(255),
+        host varchar(255),
+        user varchar(255),
+        password varchar(255)
+        )
+      `
+      conn.query(createTableStruct, [], function (err: Error, result: ResultSet) {
+        if (err) reject(err)
+        resolve(result)
+      })
+    })
+  })
+}
+
+
+
+export function addConnection(args: any) {
+  return new Promise(function (resolve, reject) {
+    const sql = `INSERT INTO connection VALUES (?,?,?,?,?);`
+    sqlite3conn.query(sql, args, function (err: Error, results: ResultSet) {
+      if (err) reject(err)
+      resolve(results)
+    })
+  })
+}
+
