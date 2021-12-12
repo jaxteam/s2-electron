f01a083084689f43129419635ef5ccd0138b8233
jaxchow <jaxchow@gmail.com>
2021-12-12 18:59:02 +0800

    chore: 修正less编译问题

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -7,13 +7,31 @@ rules.push({
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
 
+rules.push({
+  test: /\.less$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, {
+    loader: 'less-loader',
+    options: {
+      lessOptions: {
+        javascriptEnabled: true
+      },
+    },
+  },],
+});
+
+// plugins.push({
+//     "libraryName": "antd",
+//     "libraryDirectory": "lib",   // default: lib
+//     "style": true
+// })
+
 module.exports = {
-  target:'electron-renderer',
+  target: 'electron-renderer',
   module: {
     rules,
   },
   plugins: plugins,
-  externals:['canvas'],
+  externals: ['canvas'],
   optimization: {
     minimize: true,
     minimizer: [
@@ -32,8 +50,8 @@ module.exports = {
     //   "path": false,  
     //   "assert": false
     // },
-    alias:{
-      "react/jsx-runtime":require.resolve("react/jsx-runtime")
+    alias: {
+      "react/jsx-runtime": require.resolve("react/jsx-runtime")
     },
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
   },
