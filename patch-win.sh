9dd973bb25724aa356fa33f880b3298e03ec28a3
jaxchow <jaxchow@gmail.com>
2021-12-28 15:28:16 +0800

    chore：添加 window 打补丁脚本解决utils.cpp 代码问题

---
+++ patch-win.sh
@@ -0,0 +1,1 @@
+ patch -s -p0 < ./patch/java.patch node_modules/java/src/utils.cpp
--- patch.sh
+++ patch.sh
@@ -1,2 +1,1 @@
-#  patch -R -p0 < ./patch/any-db.patch node_modules/any-db/index.js
  patch -s -p0 < ./patch/any-db.patch node_modules/any-db/index.js
